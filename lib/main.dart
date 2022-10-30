b6ef08cd9be30c19a7035ecdf763fb58e1dee411
Paulo César Lins <paulocsar91@gmail.com>
2022-10-29 21:24:25 -0300

    Filtrando os Dados #01

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/meal_detail_screen.dart';
-import 'screens/categories_meals_screen.dart';
-import 'utils/app_routes.dart';
 import 'screens/tabs_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
 import 'screens/settings_screen.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+import 'utils/app_routes.dart';
+import 'models/meal.dart';
+import 'data/dummy_data.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final List<Meal> _avaliableMeals = dummyMeals;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vamos Cozinhar',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.pink,
-          secondary: Colors.amber,
-        ),
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          headline6: const TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-          )
-        )
-
-      ),
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.pink,
+            secondary: Colors.amber,
+          ),
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                  headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ))),
       routes: {
-        AppRoutes.home:(context) => const TabScreen(),
-        AppRoutes.categories_meals: (context) => const CategoriesMealsScreen(),
-        AppRoutes.meal_detail:(context) => const MealDetailScreen(),
-        AppRoutes.settings:(context) => const SettingsScreen(),
+        AppRoutes.home: (context) => const TabScreen(),
+        AppRoutes.categories_meals: (context) => CategoriesMealsScreen(_avaliableMeals),
+        AppRoutes.meal_detail: (context) => const MealDetailScreen(),
+        AppRoutes.settings: (context) => const SettingsScreen(),
       },
     );
   }
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import '../components/meal_item.dart';
 import '../models/category.dart';
-import '../data/dummy_data.dart';
+import '../models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({Key? key}) : super(key: key);
+  const CategoriesMealsScreen({Key? key, required this.meals}) : super(key: key);
+
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
-    final categoryMeals = dummyMeals.where((meal) {
+    final categoryMeals = meals.where((meal) {
       return meal.categories.contains(category.id);
     }).toList();
 
